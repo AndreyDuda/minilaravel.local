d202fdd1dbbd955a15244c73dbd7a16b65a57efa
Duda <andreyduda88@gmail.com>
2018-02-01 21:59:25 +0200

    migration and doctrine/dbal

---
+++ database/migrations/2018_02_01_193148_change_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+            $table->string('alias',100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('article', function (Blueprint $table) {
+            //
+            $table->dropColumn('alias');
+        });
+    }
+}
