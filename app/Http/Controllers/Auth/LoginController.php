f8d53f57065be8e54276a7a224b39a35020158a1
Duda <andreyduda88@gmail.com>
2018-02-06 18:57:33 +0200

    Session db

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,6 +27,7 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/home';
+
     /*protected $username = 'login';*/
     /**
      * Create a new controller instance.
@@ -34,6 +36,8 @@ class LoginController extends Controller
      */
     public function __construct()
     {
+
+        dump(Auth::user());
         $this->middleware('guest')->except('logout');
     }
     public function username()
